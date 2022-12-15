17860c57d97e3343222a6578ebbe7b15af556684
Barberousse93 <mobile.barberousse@gmail.com>
2022-12-15 09:42:42 +0100

    Ticket 2 Finaliser le formulaire : Controles HTML + 2 JS sur boutons radio et caseà cocher "conditions d'utilisation"

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -29,7 +29,52 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+// Ticket #2 Finaliser le formulaire //
+// Les champs autres que radio et checkbox sont contrôlés par HTML5
+// Déclaration des éleménts DOM utilisés
+const BtnRadio = document.getElementsByName('location');
+const CheckCondition = document.getElementById('checkbox1');
 
+// Vérification : une ville sélectionnée
+// (Un bouton radio coché)
+//Boucle sur tous les boutons radio. Si un coché, incrémentation de j
+function Location() {
+  let j = 0;
+  for (let i = 0; i < BtnRadio.length; i++) {
+    if (BtnRadio[i].checked) {
+      j++;
+    };
+  };
+  //Si j > 0 alors valide
+  if (j > 0) {
+    alert('Location VALIDE')
+    return true;
+  } else {
+    alert('Location NON VALIDE')
+    return false;
+  }
+};
+
+// Vérification conditions d'utilisation acceptées
+// (case cochée)
+function Conditions() {
+  if (CheckCondition.checked) {
+    alert('Conditions VALIDE');
+    return true;
+  } else {
+    alert('Conditions NON valide');
+    return false;
+  };
+};
+
+// Formulaire valide si toutes les verifications sont valides
 function validate() {
-  alert("VALIDE");
+  if (Location() && Conditions()) {
+    alert('Formulaire VALIDE');
+    return true;
+  } else {
+    alert('Formulaire NON VALIDE');
+    return false;
+  }
 }
+// Ticket #2 Finaliser le formulaire //
